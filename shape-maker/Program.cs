049f7ea11889d8f4a9cb3fe5ced4e76dc2746a42
Joe <joeshipp@protonmail.com>
2022-10-26 14:59:25 +0100

    shape maker
    simple shape maker using inheritance

---
+++ shape-maker/Program.cs
@@ -0,0 +1,103 @@
+﻿class shapeMaker
+{
+    public class test
+    {
+        public static void Main()
+        {
+            Shape s = new Shape(100,100);
+
+            Console.WriteLine("Shape values are " + s.X + " and " + s.Y);
+            
+            Circle c = new Circle(100,100,20.0);
+
+            Console.WriteLine("Circle values are " + c.X + " " + c.Y + " and "+ c.Radius);
+
+
+
+        }
+    }
+   public class Shape
+    {
+        public int x;
+        public int y;
+
+        public Shape()
+        {
+
+        }
+
+        public Shape (int xValue, int yValue)
+        {
+            x = xValue;
+            y = yValue;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            } 
+            set
+            {
+                x = value;
+            }
+        }
+
+        public void setValues(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+    }
+
+
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle()
+        {
+
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                if (value > 0.0)
+                    radius = value;
+            }
+        }
+
+        public Circle(int xValue, int yValue, double radiusIn)
+        {
+            X = xValue;
+            Y = yValue;
+
+            radius = radiusIn;
+
+        }
+
+        public void setData(int xValue, int yValue, double radiusIn)
+        {
+            setValues(xValue, yValue);
+            radius = radiusIn;
+        }
+    }
+}
