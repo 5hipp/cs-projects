5bcb539ff58cd07f862d6ca61f033b63aa74021b
Joe <joeshipp@protonmail.com>
2022-10-27 02:38:59 +0100

    bmi calc attempt 2
    second attempt at the bmi calculator

---
+++ bmi-calc1/BmiCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.Metrics;
+
+class BmiCalculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting...");
+    }
+
+    public BmiCalculator()
+    {
+
+        while (true)
+        {
+            Console.WriteLine("What is your gender, M or F?");
+            string gender = Console.ReadLine();
+
+            Console.WriteLine("KG or LBS?");
+            string measurement = Console.ReadLine();
+
+            Console.WriteLine("What is your weight in Kilograms?");
+            string weight = Console.ReadLine();
+            float weightf = Single.Parse(weight);
+
+            Console.WriteLine("What is your Height in Metres?");
+            string height = Console.ReadLine();
+            float heightf = Single.Parse(height);
+
+
+            Console.WriteLine("Would you like to calculate again? Y/N");
+            string cont = Console.ReadLine();
+
+            if (cont == "Y".ToLower())
+            {
+
+            }
+            else if (cont == "N".ToLower())
+            {
+                break;
+            }
+
+        }
+    }
+
+    public double BmiCalc(int weight, int height, string measurement)
+    {
+        double bmi;
+
+        if (measurement == "KG".ToLower())
+        {
+            bmi = weight / (height * height);
+            return bmi;
+        }
+
+        if (measurement == "LBS".ToLower())
+        {
+            bmi = ((height * height) * 703) / weight;
+            return bmi;
+        }
+
+        return 1;
+    }
+}
