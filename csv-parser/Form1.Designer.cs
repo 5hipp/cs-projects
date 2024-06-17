9d8bdfd79323ae1b8c0b43868f7c8384f07cc4f8
Joe <joeshipp@protonmail.com>
2024-06-17 15:11:34 +0100

    allow output and input filtering
    added buttons to filter the current output by header, implemented exporting of the file

--- csv-parser/Form1.Designer.cs
+++ csv-parser/Form1.Designer.cs
@@ -4,7 +4,8 @@
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Button btnOpenFile;
-        private System.Windows.Forms.Button btnNextLine;
+        private System.Windows.Forms.CheckedListBox checkedListBoxHeaders;
+        private System.Windows.Forms.Button btnToggleView;
         private System.Windows.Forms.DataGridView dataGridView;
 
         protected override void Dispose(bool disposing)
@@ -19,8 +20,10 @@
         private void InitializeComponent()
         {
             this.btnOpenFile = new System.Windows.Forms.Button();
-            this.btnNextLine = new System.Windows.Forms.Button();
+            this.checkedListBoxHeaders = new System.Windows.Forms.CheckedListBox();
+            this.btnToggleView = new System.Windows.Forms.Button();
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnExportCsv = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -34,31 +37,54 @@
             this.btnOpenFile.UseVisualStyleBackColor = true;
             this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
             // 
-            // btnNextLine
+            // checkedListBoxHeaders
             // 
-            this.btnNextLine.Location = new System.Drawing.Point(220, 13);
-            this.btnNextLine.Name = "btnNextLine";
-            this.btnNextLine.Size = new System.Drawing.Size(75, 23);
-            this.btnNextLine.TabIndex = 2;
-            this.btnNextLine.Text = "Next Line";
-            this.btnNextLine.UseVisualStyleBackColor = true;
-            this.btnNextLine.Click += new System.EventHandler(this.btnNextLine_Click);
+            this.checkedListBoxHeaders.FormattingEnabled = true;
+            this.checkedListBoxHeaders.Location = new System.Drawing.Point(94, 12);
+            this.checkedListBoxHeaders.Name = "checkedListBoxHeaders";
+            this.checkedListBoxHeaders.Size = new System.Drawing.Size(200, 94);
+            this.checkedListBoxHeaders.TabIndex = 1;
+            // 
+            // btnToggleView
+            // 
+            this.btnToggleView.Location = new System.Drawing.Point(13, 42);
+            this.btnToggleView.Name = "btnToggleView";
+            this.btnToggleView.Size = new System.Drawing.Size(75, 23);
+            this.btnToggleView.TabIndex = 2;
+            this.btnToggleView.Text = "Apply Filter";
+            this.btnToggleView.UseVisualStyleBackColor = true;
+            this.btnToggleView.Click += new System.EventHandler(this.btnToggleView_Click);
             // 
             // dataGridView
             // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView.Location = new System.Drawing.Point(13, 113);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(775, 395);
+            this.dataGridView.Size = new System.Drawing.Size(775, 325);
             this.dataGridView.TabIndex = 3;
+            // 
+            // btnExportCsv
+            // 
+            this.btnExportCsv.Location = new System.Drawing.Point(13, 72);
+            this.btnExportCsv.Name = "btnExportCsv";
+            this.btnExportCsv.Size = new System.Drawing.Size(75, 23);
+            this.btnExportCsv.TabIndex = 4;
+            this.btnExportCsv.Text = "Export CSV";
+            this.btnExportCsv.UseVisualStyleBackColor = true;
+            this.btnExportCsv.Click += new System.EventHandler(this.btnExportCsv_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExportCsv);
             this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.btnNextLine);
+            this.Controls.Add(this.btnToggleView);
+            this.Controls.Add(this.checkedListBoxHeaders);
             this.Controls.Add(this.btnOpenFile);
             this.Name = "Form1";
             this.Text = "CSV Viewer";
@@ -66,6 +92,8 @@
             this.ResumeLayout(false);
 
         }
+
+        private System.Windows.Forms.Button btnExportCsv;
     }
 }
 
--- csv-parser/Form1.cs
+++ csv-parser/Form1.cs
@@ -14,8 +14,9 @@ namespace csv_parser
 {
     public partial class Form1 : Form
     {
-        private int currentLineIndex = 1;
-        private string[] csvLines;
+        private bool isFilteredView = false;
+        private DataTable fullCsvData;
+        private DataTable filteredCsvData;
 
         public Form1()
         {
@@ -30,35 +31,31 @@ namespace csv_parser
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     string filePath = openFileDialog.FileName;
-                    csvLines = File.ReadAllLines(filePath);
-                    DataTable csvData = ReadCsvFile(csvLines);
-                    dataGridView.DataSource = csvData;
+                    fullCsvData = ReadCsvFile(filePath);
+                    dataGridView.DataSource = fullCsvData;
+                    PopulateCheckedListBox(fullCsvData);
                 }
             }
         }
 
-        private void btnOpenSingleLine_Click(object sender, EventArgs e)
+        private void btnToggleView_Click(object sender, EventArgs e)
         {
-            if (csvLines != null)
+            if (isFilteredView)
             {
-                DataTable csvData = ReadSingleLineCsvFile(csvLines, currentLineIndex);
-                dataGridView.DataSource = csvData;
+                dataGridView.DataSource = fullCsvData;
+                btnToggleView.Text = "Apply Filter";
             }
+            else
+            {
+                ApplyFilter();
+            }
+            isFilteredView = !isFilteredView;
         }
 
-        private void btnNextLine_Click(object sender, EventArgs e)
-        {
-            if (csvLines != null && currentLineIndex < csvLines.Length - 1)
-            {
-                currentLineIndex++;
-                DataTable csvData = ReadSingleLineCsvFile(csvLines, currentLineIndex);
-                dataGridView.DataSource = csvData;
-            }
-        }
-
-        private DataTable ReadCsvFile(string[] lines)
+        private DataTable ReadCsvFile(string filePath)
         {
             DataTable dt = new DataTable();
+            string[] lines = File.ReadAllLines(filePath);
 
             if (lines.Length > 0)
             {
@@ -85,30 +82,102 @@ namespace csv_parser
             return dt;
         }
 
-        private DataTable ReadSingleLineCsvFile(string[] lines, int lineIndex)
+        private void ApplyFilter()
         {
-            DataTable dt = new DataTable();
-
-            if (lines.Length > 0 && lineIndex < lines.Length)
+            List<string> selectedHeaders = checkedListBoxHeaders.CheckedItems.Cast<string>().ToList();
+            if (selectedHeaders.Count > 0)
             {
-                // Adding columns
-                string[] headerLabels = lines[0].Split(',');
-                foreach (string headerWord in headerLabels)
-                {
-                    dt.Columns.Add(new DataColumn(headerWord));
-                }
+                filteredCsvData = FilterCsvData(fullCsvData, selectedHeaders);
+                dataGridView.DataSource = filteredCsvData;
+                btnToggleView.Text = "Show Full Table";
+            }
+            else
+            {
+                MessageBox.Show("Please select at least one header to filter by.");
+            }
+        }
 
-                // Adding the specific row
-                string[] dataWords = lines[lineIndex].Split(',');
-                DataRow dr = dt.NewRow();
-                for (int col = 0; col < headerLabels.Length; col++)
-                {
-                    dr[col] = dataWords[col];
-                }
-                dt.Rows.Add(dr);
+        private DataTable FilterCsvData(DataTable dataTable, List<string> columns)
+        {
+            DataTable filteredTable = new DataTable();
+            foreach (string column in columns)
+            {
+                filteredTable.Columns.Add(new DataColumn(column));
             }
 
-            return dt;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                DataRow newRow = filteredTable.NewRow();
+                foreach (string column in columns)
+                {
+                    newRow[column] = row[column];
+                }
+                filteredTable.Rows.Add(newRow);
+            }
+
+            return filteredTable;
+        }
+
+        private void PopulateCheckedListBox(DataTable dataTable)
+        {
+            checkedListBoxHeaders.Items.Clear();
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                checkedListBoxHeaders.Items.Add(column.ColumnName);
+            }
+        }
+
+        private void btnExportCsv_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.DataSource != null)
+            {
+                DataTable dt = (DataTable)dataGridView.DataSource;
+
+                // Prompt user for file save location
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                saveFileDialog.FileName = "ExportedData.csv";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the file path
+                    string filePath = saveFileDialog.FileName;
+
+                    // Write CSV file
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
+                        {
+                            // Write headers
+                            foreach (DataColumn column in dt.Columns)
+                            {
+                                sw.Write(column.ColumnName + ",");
+                            }
+                            sw.WriteLine();
+
+                            // Write data rows
+                            foreach (DataRow row in dt.Rows)
+                            {
+                                object[] array = row.ItemArray;
+                                for (int i = 0; i < array.Length; i++)
+                                {
+                                    sw.Write(array[i].ToString() + ",");
+                                }
+                                sw.WriteLine();
+                            }
+                        }
+
+                        MessageBox.Show("CSV file exported successfully.", "Export Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error exporting CSV file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No data to export.", "Export Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
