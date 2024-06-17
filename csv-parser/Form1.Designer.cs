cd7a4b10ba50774ee464cc583429ffd0311d4d1d
Joe <joeshipp@protonmail.com>
2024-06-17 14:48:33 +0100

    upload files
    init commit, file upload accepts csv file, can print by one line or full file

---
+++ csv-parser/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace csv_parser
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnNextLine;
+        private System.Windows.Forms.DataGridView dataGridView;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnNextLine = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(13, 13);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open CSV";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnNextLine
+            // 
+            this.btnNextLine.Location = new System.Drawing.Point(220, 13);
+            this.btnNextLine.Name = "btnNextLine";
+            this.btnNextLine.Size = new System.Drawing.Size(75, 23);
+            this.btnNextLine.TabIndex = 2;
+            this.btnNextLine.Text = "Next Line";
+            this.btnNextLine.UseVisualStyleBackColor = true;
+            this.btnNextLine.Click += new System.EventHandler(this.btnNextLine_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(775, 395);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnNextLine);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "CSV Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ csv-parser/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace csv_parser
+{
+    public partial class Form1 : Form
+    {
+        private int currentLineIndex = 1;
+        private string[] csvLines;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    csvLines = File.ReadAllLines(filePath);
+                    DataTable csvData = ReadCsvFile(csvLines);
+                    dataGridView.DataSource = csvData;
+                }
+            }
+        }
+
+        private void btnOpenSingleLine_Click(object sender, EventArgs e)
+        {
+            if (csvLines != null)
+            {
+                DataTable csvData = ReadSingleLineCsvFile(csvLines, currentLineIndex);
+                dataGridView.DataSource = csvData;
+            }
+        }
+
+        private void btnNextLine_Click(object sender, EventArgs e)
+        {
+            if (csvLines != null && currentLineIndex < csvLines.Length - 1)
+            {
+                currentLineIndex++;
+                DataTable csvData = ReadSingleLineCsvFile(csvLines, currentLineIndex);
+                dataGridView.DataSource = csvData;
+            }
+        }
+
+        private DataTable ReadCsvFile(string[] lines)
+        {
+            DataTable dt = new DataTable();
+
+            if (lines.Length > 0)
+            {
+                // Adding columns
+                string[] headerLabels = lines[0].Split(',');
+                foreach (string headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+
+                // Adding rows
+                for (int r = 1; r < lines.Length; r++)
+                {
+                    string[] dataWords = lines[r].Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int col = 0; col < headerLabels.Length; col++)
+                    {
+                        dr[col] = dataWords[col];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            return dt;
+        }
+
+        private DataTable ReadSingleLineCsvFile(string[] lines, int lineIndex)
+        {
+            DataTable dt = new DataTable();
+
+            if (lines.Length > 0 && lineIndex < lines.Length)
+            {
+                // Adding columns
+                string[] headerLabels = lines[0].Split(',');
+                foreach (string headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+
+                // Adding the specific row
+                string[] dataWords = lines[lineIndex].Split(',');
+                DataRow dr = dt.NewRow();
+                for (int col = 0; col < headerLabels.Length; col++)
+                {
+                    dr[col] = dataWords[col];
+                }
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+    }
+}
